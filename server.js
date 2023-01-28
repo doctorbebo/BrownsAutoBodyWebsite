bb9902c311979778c63a721abd69e22fa10286d9
Byron Brown <byronrb92@gmail.com>
2023-01-28 12:50:50 -0500

    Add console log to server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import exp from "constants"
 import express from "express"
 const app = express()
+const port = 3000
 
 app.use(express.static("public"))
 
-app.listen(3000)
+app.listen(port)
+console.log("listening on port: " + port)
